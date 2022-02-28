b5c16847ac75f4c1451645c43055bb407d5b29a4
Premraj Deore <premrajdeore27@gmail.com>
2022-02-28 15:16:46 +0530

    initchanges

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './index.css';
+import Form from './/assignment_2/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
